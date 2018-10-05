471f443ab452bc1e22ee92c70b7eacc2a06aafec
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2018-10-05 15:13:48 +0300

    Removed dependency to codelyzer

--- tslint.js
+++ tslint.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   rulesDirectory: [
-    path.join(path.dirname(require.resolve('codelyzer')), './'),
     path.join(path.dirname(require.resolve('tslint-microsoft-contrib')), './'),
     path.join(path.dirname(require.resolve('tslint-sonarts')), 'lib/rules'),
   ],
