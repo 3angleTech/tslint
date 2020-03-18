2e5809f67cf2279876b61cb93575ce79599235c0
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2020-03-18 07:45:43 +0200

    #3 Reorganize and update lint rules.

--- angular/index.js
+++ angular/index.js
@@ -2,18 +2,9 @@ const path = require('path');
 
 module.exports = {
   rulesDirectory: [
-    path.join(path.dirname(require.resolve('codelyzer')), './'),
+    path.dirname(require.resolve('codelyzer'))
   ],
-  rules: {
-    "no-output-on-prefix": true,
-    "use-input-property-decorator": true,
-    "use-output-property-decorator": true,
-    "use-host-property-decorator": true,
-    "no-input-rename": true,
-    "no-output-rename": true,
-    "use-life-cycle-interface": true,
-    "use-pipe-transform-interface": true,
-    "component-class-suffix": true,
-    "directive-class-suffix": true,
-  },
+  extends: [
+    './tslint-codelyzer.json'
+  ]
 };
